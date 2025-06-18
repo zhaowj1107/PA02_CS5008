483cacc6b9acf635447ef165e249569d82b06dda
Weijian Zhao <zhaowj1001@login-el8.khoury.northeastern.edu>
2025-06-18 18:18:11 -0400

    test04 update

--- test04_singleasterisk.c
+++ test04_singleasterisk.c
@@ -16,7 +16,7 @@ SinglyNode* create_singly_node(int data);
 void print_list(SinglyNode* head);
 
 // TODO
-void insert_at_head(SinglyNode** head, int data) {
+void insert_at_head(SinglyNode* head, int data) {
     // write your code here.
     if (head == NULL) {
 	return;
@@ -26,20 +26,20 @@ void insert_at_head(SinglyNode** head, int data) {
 	return;
     }
     new_node->data = data;
-    new_node->next = *head;
-    *head = new_node;
+    new_node->next = head;
+    head = new_node;
 }
 
 int main() {
     SinglyNode* head = create_singly_node(100);
-    insert_at_head(&head, 10);
-    insert_at_head(&head, 20);
+    insert_at_head(head, 10);
+    insert_at_head(head, 20);
     printf("Linked list output: ");
-    print_list(head);  // Output: 20 10 100
+    print_list(head);  // Output: 100
 
     // Free space (short-cut)
-    free(head->next->next);
-    free(head->next);
+    // free(head->next->next);
+    // free(head->next);
     free(head);
 
     return 0;
